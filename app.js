5a578e245c08b015883488ba13ebd57c8eefd0b3
Nike159 <nikeblack159@gmail.com>
2023-01-26 16:43:36 -0600

    Added JS
    Need to fix time bug where it is set hours ahead. Also need to fix bug where you need to click search button twice to get total score to render.

--- app.js
+++ app.js
@@ -1,69 +1,85 @@
 let currentDate = new Date().toJSON().slice(0, 10);
 console.log(currentDate);
+let homeS = 0;
+let awayS = 0;
+var total = 0;
 
 
-let homeScore = {
+let totalScore = {
     
-    fetchScore: function (team_id) {
-        fetch("https://www.balldontlie.io/api/v1/games?start_date=2023-01-19&end_date=2023-01-19&team_ids[]=" + team_id +"")
+    homeScore: function (team_id) {
+        fetch("https://www.balldontlie.io/api/v1/games?start_date=2023-01-25&end_date=2023-01-25&team_ids[]=" + team_id +"")
         .then((data)=>{
             return data.json();
         }).then((completedata)=>{
+            this.homeS = completedata.data[0].home_team_score;
             console.log(completedata.data[0].home_team_score);
             document.querySelector(".homeScore").innerText = completedata.data[0].home_team_score;
         });
+        
 
 
 },
 
+    hsearch: function () {
+        this.homeScore(document.querySelector(".hsearch").value);
+},
+
+    awayScore: function (team_id) {
+        fetch("https://www.balldontlie.io/api/v1/games?start_date=2023-01-25&end_date=2023-01-25&team_ids[]=" + team_id +"")
+        .then((data)=>{
+            return data.json();
+        }).then((completedata)=>{
+            this.awayS = completedata.data[0].visitor_team_score;
+            console.log(completedata.data[0].visitor_team_score);
+            document.querySelector(".awayScore").innerText = completedata.data[0].visitor_team_score;
+        });
+    },
+    asearch: function () {
+        this.awayScore(document.querySelector(".asearch").value);
+        
+    },
+
+    
+
+    score: function () {
+        var total= 0;
+        var total = this.homeS + this.awayS;
+        document.querySelector("#total").innerText = total;
+    },
 
 
-
-
-
-
-search: function () {
-    this.fetchScore(document.querySelector(".search").value);
-}};
+    
+};
 
 document
 .querySelector(".homeButton")
 .addEventListener("click", function () {
-    homeScore.search();
+    totalScore.hsearch();
 });
-
-
-
-let awayScore = {
-    
-    fetchScore: function (team_id) {
-        fetch("https://www.balldontlie.io/api/v1/games?start_date=2023-01-19&end_date=2023-01-19&team_ids[]=" + team_id +"")
-        .then((data)=>{
-            return data.json();
-        }).then((completedata)=>{
-            console.log(completedata.data[0].visitor_team_score);
-            document.querySelector(".awayScore").innerText = completedata.data[0].visitor_team_score;
-        });
-
-
-},
-
-
-
-
-
-
-
-search: function () {
-    this.fetchScore(document.querySelector(".search").value);
-}};
 
 document
 .querySelector(".awayButton")
 .addEventListener("click", function () {
-    awayScore.search();
+    totalScore.asearch();
 });
 
-var total = awayScore + homeScore;
-console.log(total);
+document
+.querySelector(".awayButton")
+.addEventListener("click", function () {
+    totalScore.score();
+});
+
+document
+.querySelector(".homeButton")
+.addEventListener("click", function () {
+    totalScore.score();
+});
+
+
+
+
+
+
+    
 
