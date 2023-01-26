7e311dff29440e14700195f8a35b069063c59733
Nike159 <108959165+Nike159@users.noreply.github.com>
2023-01-26 16:49:39 -0600

    Delete note.js

--- note.js
+++
@@ -1,4 +0,0 @@
-.then(response => response.json())
-.then(data => console.log(data.data[0].home_team_score)) 
-fetch("https://www.balldontlie.io/api/v1/games?start_date=" + currentDate + "&end_date=" + currentDate + "&team_ids[]=" + team_id +"")
-document.querySelector(".homeScore").innerText = score;
